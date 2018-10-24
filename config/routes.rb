215f4aae14bbfa39c66ca5cbdad51a45c84dc6b3
3413 <3413@example.com>
2018-10-24 11:08:13 +0900

    パスワードの暗号化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  post '/ee_searchs', to: 'ee_report_searchs#search'
+  post '/et_searchs', to: 'et_report_searchs#search'
   get 'et_report_searchs/index'
   get 'et_report_searchs/show'
   get 'ee_report_searchs/index'
