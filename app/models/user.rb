6d4d4589c120c7083ab4fccc3d2c8da23b7b3e32
3413 <3413@example.com>
2018-10-26 11:49:01 +0900

    ユーザーマスタのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ApplicationRecord
     has_secure_password 
+    validates :user_id, uniqueness: {message: "重複したユーザＩＤは登録できません"}
+
+    validate :error_check
+    def error_check
+      if user_id.blank?
+        errors[:base] << 'ユーザＩＤは必ず入力してください'
+     end        
+      if name.blank?
+        errors[:base] << '名前は必ず入力してください'
+      end
+      if annual.blank?
+        errors[:base] << '年度は必ず入力してください'
+      end
+      if password.blank?
+        errors[:base] << 'パスワードは必ず入力してください'
+     end        
+
+
+    end
 end
