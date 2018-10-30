12c5eb011b4430f9879f0245696a3c4b3b70afbc
3413 <3413@example.com>
2018-10-30 10:02:06 +0900

    バリデーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    has_secure_password 
     validates :user_id, uniqueness: {message: "重複したユーザＩＤは登録できません"}
+    validates :annual, length: { is: 4, message: "は必ず4桁で入力してください" }
+    validates :user_id, length: { is: 9, message: "ユーザIDは必ず9桁で登録してください" }
 
     validate :error_check
     def error_check
@@ -17,6 +18,7 @@ class User < ApplicationRecord
         errors[:base] << 'パスワードは必ず入力してください'
      end        
 
-
     end
+    has_secure_password     
 end
+
