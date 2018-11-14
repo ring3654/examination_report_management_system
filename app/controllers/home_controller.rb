38a1b60dfc18a0eba1eccac544d2a3b798ca40a0
3413 <3413@example.com>
2018-11-14 12:21:31 +0900

    ユーザー権限の設定

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,13 @@ class HomeController < ApplicationController
   before_action :login_check
 
   def retrieval_top
+      @user_authority = User.find_by(user_id: session[:login_id]).authority
   end
   
   def top
     @ee_reports = EeReport.where(student_id: session[:login_id],approval_flg: 0)
     @et_reports = EtReport.where(student_id: session[:login_id],approval_flg: 0)
+    @user_authority = User.find_by(user_id: session[:login_id]).authority
   end
 
 end
