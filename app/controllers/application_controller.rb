2fa7b9adb453264193ff0db5a4c33c0456b5f9c1
3413 <3413@example.com>
2019-01-23 11:53:36 +0900

    ページアクセスの制限の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,18 @@ class ApplicationController < ActionController::Base
         #もし、sessionのログインIDがなかったら、
         if session[:login_id].blank?
         #ログイン画面に移動
-        redirect_to welcome_login_path, notice: "ログインされてないか、セッションが切れています。再度ログインしてください。", layout: nil
+          redirect_to welcome_login_path, notice: "ログインされてないか、セッションが切れています。再度ログインしてください。", layout: nil
         end
     end
+
+    def authority_check
+        authority = User.find_by(user_id: session[:login_id]).authority
+        if authority == 2 || authority == 3
+            flash[:error] = "あなたにはそのページにアクセスする権限がありません。"
+            redirect_to home_top_path
+        end
+    end
+
 
     def before_controller_check
         @path = Rails.application.routes.recognize_path(request.referer)
--- app/controllers/unapproved_reports_controller.rb
+++ app/controllers/unapproved_reports_controller.rb
@@ -1,6 +1,8 @@
 class UnapprovedReportsController < ApplicationController
   before_action :login_check
   before_action :before_controller_check
+  before_action :authority_check
+
 
   def index
     @ee_reports = EeReport.where(approval_flg: 0)
